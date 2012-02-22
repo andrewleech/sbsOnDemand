65477ae3593f0fcd29738b4568d0809ed2d5dea8
Michael van der Kolff <mvanderkolff@gmail.com>
2012-02-22 19:41:27 +1100

    Correct version so that it builds with setup.py bdist_msi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='SbsOnDemand',
-      version='20120222',
+      version='0.0.20120222',
       description='SbsOnDemand library',
       author='Michael van der Kolff',
       author_email='mvanderkolff@gmail.com',
