668b9685b245571329611b5a74539fc4afe82ff6
Michael van der Kolff <mvanderkolff@gmail.com>
2012-02-22 19:18:34 +1100

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SbsOnDemand',
+      version='20120222',
+      description='SbsOnDemand library',
+      author='Michael van der Kolff',
+      author_email='mvanderkolff@gmail.com',
+      url='https://github.com/mvanderkolff/sbsOnDemand',
+      packages=['SbsOnDemand'],
+     )
