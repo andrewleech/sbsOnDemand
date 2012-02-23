34180a8405b721f3d5df5dd63469d5c794565139
Michael van der Kolff <mvanderkolff@gmail.com>
2012-02-23 20:36:59 +1100

    Add scripts/ to the setup.py.  This has no real effect for windows; something ought to be done about this...

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='SbsOnDemand',
-      version='0.0.20120222',
-      description='SbsOnDemand library',
+      version='0.0.20120223',
       author='Michael van der Kolff',
       author_email='mvanderkolff@gmail.com',
       url='https://github.com/mvanderkolff/sbsOnDemand',
       packages=['SbsOnDemand'],
+      scripts=['scripts/sbs-downloader-ondemand'],
+      description='Sbs (Australian TV Network) On-Demand downloader library',
+      license='GPL 3.0',
      )
