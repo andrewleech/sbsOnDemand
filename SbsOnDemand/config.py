31e32edf1b11cd07f96e4f262fc926de33506d3c
Ye Chuah <yechuah@gmail.com>
2013-01-03 22:25:03 +1100

    add documentary feed

--- SbsOnDemand/config.py
+++ SbsOnDemand/config.py
@@ -29,5 +29,6 @@ DEFAULT_FEEDS = [
     {"name": "Events", "feedId": "section-events"},
     {"name": "Last Chance", "feedId":"videos-lastchance"},
     {"name": "Featured Programs", "feedId":"featured-programs-prod"},
+    {"name": "Documentary", "url": 'http://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-sbstv?form=json&byCategories=Documentary|Factual|Factual%2FAnimation|Factual%2FArchaeology|Factual%2FBiography|Factual%2FBusiness+and+Economics|Factual%2FCulture+and+Society|Factual%2FDocu-drama|Factual%2FHealth+%2B+Science+and+Technology|Factual%2FHistory|Factual%2FLifestyle|Factual%2FPolitics+and+Current+Affairs|Factual%2FNature+and+Environment|Factual%2FSport|Factual%2FSexuality%2CSection%2FClips|Section%2FPrograms&count=true&range=1-40&defaultThumbnailAssetType=Thumbnail'},
     {"name": "Featured Clips", "feedId":"featured-clips"}
 ]
